d2a548e4443bec5203635468aa41c333c356411c
CHANDRA <chandrawardhana21@gmail.com>
2023-01-14 05:56:36 +0700

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using BBlazor.Data;
+using BootstrapBlazor.Components;
+
+namespace BBlazor;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+		builder.Services.AddBootstrapBlazor();
+
+		
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
---
+++ Shared/AttributeItem.cs
@@ -0,0 +1,6 @@
+﻿namespace BBlazor
+{
+    internal class AttributeItem
+    {
+    }
+}
